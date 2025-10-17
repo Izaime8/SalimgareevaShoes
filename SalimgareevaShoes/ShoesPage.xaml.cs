fbb7c29b34f9a26679f02ad7d57d6bb6a79e0164
Izabel Salimgareeva <Izabelsal60@gmail.com>
2025-10-17 14:38:36 +0500

    Сортировка, поиск, фильтрация

--- SalimgareevaShoes/ShoesPage.xaml.cs
+++ SalimgareevaShoes/ShoesPage.xaml.cs
@@ -24,18 +24,85 @@ namespace SalimgareevaShoes
         {
             InitializeComponent();
 
+
+            SortCB.SelectedIndex = 0;
+            SupplersCB.SelectedIndex = 0;
             UpdateProduct();
+
         }
 
         void UpdateProduct()
         {
             var currentProducts = SalimgarevaShoesEntities.GetContext().Products.ToList();
+
+            var searchedProducts = currentProducts;
+            foreach (string s in SearchTB.Text.Split())
+            {
+                searchedProducts = searchedProducts.Where(p => p.ProductArticle.ToLower().Contains(s.ToLower()) ||
+                p.ProductName.ToLower().Contains(s.ToLower()) ||
+                p.ProductUnitMeasure.ToLower().Contains(s.ToLower()) ||
+                p.Supplers.SupplerName.ToLower().Contains(s.ToLower()) ||
+                p.Manufacturers.ManufacturerName.ToLower().Contains(s.ToLower()) ||
+                p.Categories.CategoryName.ToLower().Contains(s.ToLower()) ||
+                p.ProductDescription.ToLower().Contains(s.ToLower())).ToList();
+            }
+
+            if (searchedProducts != null)
+                currentProducts = searchedProducts;
+
+
+
+            if (SupplersCB.SelectedIndex == 1)
+            {
+                currentProducts = currentProducts.Where(p => p.SupplerID == 1).ToList();
+            }
+            if (SupplersCB.SelectedIndex == 2)
+            {
+                currentProducts = currentProducts.Where(p => p.SupplerID == 2).ToList();
+            }
+
+
+
+            if (SortCB.SelectedIndex == 1)
+            {
+                currentProducts = currentProducts.OrderBy(p => p.ProductQuantityInStock).ToList();
+            }
+            if (SortCB.SelectedIndex == 2)
+            {
+                currentProducts = currentProducts.OrderByDescending(p => p.ProductQuantityInStock).ToList();
+            }
+
+
+
+
+
+
             ShoesListView.ItemsSource = currentProducts;
 
         }
 
         private void AddButton_Click(object sender, RoutedEventArgs e)
         {
+            UpdateProduct();
+        }
+
+        private void SearchTB_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            UpdateProduct();
+
+        }
+
+       
+
+        private void SortCB_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            UpdateProduct();
+
+        }
+
+        private void SupplereCB_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            UpdateProduct();
 
         }
     }
